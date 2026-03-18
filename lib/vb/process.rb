854c8272609df23cd46cef93c0b458dd9febc5b1
Jonas Brusman <jonas@brusman.se>
2026-03-18 10:50:23 +0100

    Remove dead Process#in_use? and #in_use_dirs

--- lib/vb/process.rb
+++ lib/vb/process.rb
@@ -2,26 +2,11 @@
 
 module VB
   class Process
-    def in_use?(workspace_dir:)
-      ps_output.include?(workspace_dir)
-    end
-
-    def in_use_dirs(workspace_dirs:)
-      output = ps_output
-      workspace_dirs.select { |dir| output.include?(dir) }
-    end
-
     def alive?(pid:)
       ::Process.kill(0, Integer(pid))
       true
     rescue Errno::ESRCH, Errno::EPERM
       false
-    end
-
-    private
-
-    def ps_output
-      `ps aux`
     end
   end
 end
--- test/vb/process_test.rb
+++ test/vb/process_test.rb
@@ -1,40 +1,6 @@
 # frozen_string_literal: true
 
 class VB::ProcessTest < TLDR
-  def test_in_use_returns_true_when_dir_appears_in_ps
-    process = VB::Process.new
-    def process.ps_output
-      "501 1234 ruby /home/user/myrepo-swift-falcon/some_script.rb\n"
-    end
-    assert process.in_use?(workspace_dir: "/home/user/myrepo-swift-falcon")
-  end
-
-  def test_in_use_returns_false_when_dir_not_in_ps
-    process = VB::Process.new
-    def process.ps_output
-      "501 1234 ruby /other/path/script.rb\n"
-    end
-    refute process.in_use?(workspace_dir: "/home/user/myrepo-swift-falcon")
-  end
-
-  def test_in_use_dirs_returns_only_matching_dirs
-    process = VB::Process.new
-    def process.ps_output
-      "501 1234 ruby /repo-alpha/script.rb\n501 5678 node /repo-beta/server.js\n"
-    end
-    result = process.in_use_dirs(workspace_dirs: ["/repo-alpha", "/repo-gamma"])
-    assert_equal ["/repo-alpha"], result
-  end
-
-  def test_in_use_dirs_returns_empty_when_none_match
-    process = VB::Process.new
-    def process.ps_output
-      "501 9999 bash\n"
-    end
-    result = process.in_use_dirs(workspace_dirs: ["/repo-alpha", "/repo-beta"])
-    assert_equal [], result
-  end
-
   def test_alive_returns_true_for_current_process
     assert VB::Process.new.alive?(pid: Process.pid)
   end
